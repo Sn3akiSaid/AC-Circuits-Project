578a7155611f3c16537354acfdb89f151a4be688
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-05-10 22:08:13 +0100

    Created derived resistor class, basic implementation

---
+++ inductor.h
@@ -0,0 +1,13 @@
+#ifndef INDUCTOR_H
+#define INDUCTOR_H
+#include"components.h"
+#include<iostream>
+#include<memory>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<initializer_list>
+
+
+
+#endif
---
+++ resistor.h
@@ -0,0 +1,32 @@
+#ifndef RESISTOR_H
+#define RESISTOR_H
+#include"components.h"
+#include<iostream>
+#include<memory>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<initializer_list>
+
+class Resistor : public Component {
+public:
+    Resistor(std::string name, double value) : name(name), value(value) {}
+    ~Resistor() override = default;
+
+    std::string getName() const override {
+        return name;
+    }
+
+    double getValue() const override {
+        return value;
+    }
+
+    void setValue(double newValue) override {
+        value = newValue;
+    }
+
+    void print() const override {
+        std::cout << "Resistor: " << name << ", Value: " << value << " Ohm" << std::endl;
+    }}
+
+#endif
