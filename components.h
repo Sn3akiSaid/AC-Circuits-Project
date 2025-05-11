34a7618e4cd783c6ee9bf02fe869f35dc4fe2b5d
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-05-11 22:48:39 +0100

    Implemented getters, although not sure on impedance magnitude getter just yet.

--- components.h
+++ components.h
@@ -21,7 +21,13 @@ public:
   Component(const std::string &compType = "none", std::complex<double> impIn = 0.0, double magnIn = 0.0, double freqIn=0.0, double phsDiffInn = 0.0);
   virtual ~Component() = default;
 
-  
+  // Getters
+  std::string getType() const {return componentType;}
+  std::complex<double> getImp() const {return Impedance;}
+  double getMagn() const {return magnitudeOfImpedance;}
+  double getFreq() const {return Frequency;}
+  double getPhsDiff() const {return phaseDifference;}
+
 
 
 };
