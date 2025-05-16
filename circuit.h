9a6ddc6b3ad94c3e832de255a99632918f85d106
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-05-16 23:25:05 +0100

    Added enum class for the type of circuit- series / parallel

--- circuit.h
+++ circuit.h
@@ -7,9 +7,13 @@
 #include<vector>
 #include<memory>
 
+enum class ConnectionType {Series, Parallel};
+
 class Circuit{
 private:
   std::vector<std::unique_ptr<Components>> elements;
+  ConnectionType connType;
+  std::string name;
 
 public:
   void addComponent(std::unique_ptr<Components> component);
