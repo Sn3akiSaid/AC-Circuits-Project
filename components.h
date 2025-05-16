0ec67966d5213331ff2160e2855ca5e1f14cf5d1
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2025-05-16 17:54:21 +0100

    Implement copy operations using virtual methods

--- components.h
+++ components.h
@@ -10,30 +10,45 @@
 
 class Components {
 protected:
-  std::string componentType;
-  std::complex<double> Impedance;
-  double magnitudeOfImpedance;
-  double Frequency;
-  double phaseDifference;
-  
+	std::string componentType;
+	std::complex<double> Impedance;
+	double magnitudeOfImpedance;
+	double Frequency;
+	double phaseDifference;
+	
 
 public:
-  Components(const std::string &compType = "none", std::complex<double> impIn = 0.0, double magnIn = 0.0, double freqIn=0.0, double phsDiffIn = 0.0);
-  virtual ~Components() = default;
+	// Constructor declaration
+	Components(const std::string &compType = "none",
+						 std::complex<double> impIn = 0.0, 
+						 double magnIn = 0.0,
+						 double freqIn=0.0,
+						 double phsDiffIn = 0.0);
 
-  // Getters
-  virtual std::string getType() const = 0;
-  virtual std::complex<double> getImp() const = 0;
-  virtual double getMagn() const = 0;
-  virtual double getFreq() const = 0;
-  virtual double getPhsDiff() const = 0;
+	// Using Rule of Five
 
-  // Setters
-  virtual void setType(std::string &compType) = 0;
-  virtual void setImp(std::complex<double> impIn) = 0;
-  virtual void setMagn(double magnIn) = 0;
-  virtual void setFreq(double freqIn) = 0;
-  virtual void setPhsDiff(double phsDiffIn) = 0;
+	// Virtual destructor
+	virtual ~Components() = default;
+
+	// Copy Operations
+	Components(const Components& other) = default; 							// Non virtual copy constructor
+	virtual Components& operator=(const Components& other) = 0; // Virtual copy assignment
+
+	// Move operations
+
+	// Getters
+	virtual std::string getType() const = 0;
+	virtual std::complex<double> getImp() const = 0;
+	virtual double getMagn() const = 0;
+	virtual double getFreq() const = 0;
+	virtual double getPhsDiff() const = 0;
+
+	// Setters
+	virtual void setType(std::string &compType) = 0;
+	virtual void setImp(std::complex<double> impIn) = 0;
+	virtual void setMagn(double magnIn) = 0;
+	virtual void setFreq(double freqIn) = 0;
+	virtual void setPhsDiff(double phsDiffIn) = 0;
 
 
 
